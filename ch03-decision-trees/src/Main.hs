a50210981f69a99618d06387f82ba337559d7d71
Richard Cook <rcook@rcook.org>
2016-11-25 16:13:01 -0800

    Tidy up main function

--- ch03-decision-trees/src/Main.hs
+++ ch03-decision-trees/src/Main.hs
@@ -1,19 +1,16 @@
 module Main (main) where
 
-import           Ch03DecisionTrees.DecisionTree hiding (F, Leaf, Node)
-import qualified Ch03DecisionTrees.DecisionTree as DT
-import           DataFiles
-import           Diagrams.Backend.SVG.CmdLine
+import           Ch03DecisionTrees.DecisionTree
 import           MLUtil
 import           MLUtil.Graphics
 
 dataSet :: [Record]
 dataSet =
-    [ (DT.F <$> [1, 1], C "yes")
-    , (DT.F <$> [1, 1], C "yes")
-    , (DT.F <$> [1, 0], C "no")
-    , (DT.F <$> [0, 1], C "no")
-    , (DT.F <$> [0, 1], C "no")
+    [ (F <$> [1, 1], C "yes")
+    , (F <$> [1, 1], C "yes")
+    , (F <$> [1, 0], C "no")
+    , (F <$> [0, 1], C "no")
+    , (F <$> [0, 1], C "no")
     ]
 
 labels :: [Label]
@@ -25,20 +22,5 @@ renderFigures = do
     let c = flowchart (mkDecisionTree dataSet labels)
     renderFlowchartSVG "flowchart.svg" c
 
-data StringFeature = F String
-instance ArrowLabel StringFeature where
-    arrowLabel (F s) = Just s
-
-node = Node . L
-leaf = Leaf . C
-
-example :: Diagram
-example =
-    let tree = node "label0"
-            [ (node "label1" [(leaf "N0", F "F2"), (leaf "N1", F "F3"), (leaf "N2", F "F4")], F "F0")
-            , (node "label2" [(node "label1" [(leaf "N0", F "F2"), (leaf "N1", F "F3"), (leaf "N2", F "F4")], F "F0"), (leaf "N4", F "F6"), (leaf "N5", F "F7")], F "F1")
-            ]
-    in flowchart tree
-
 main :: IO ()
-main = renderFigures >> mainWith example
+main = renderFigures
