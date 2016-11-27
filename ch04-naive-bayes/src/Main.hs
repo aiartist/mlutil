3f3cc8b4488dcf34e8f53f6e3ebb048cab881db8
Richard Cook <rcook@rcook.org>
2016-11-26 23:23:33 -0800

    Finish initial implementation of trainNB0

--- ch04-naive-bayes/src/Main.hs
+++ ch04-naive-bayes/src/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import qualified Data.Set as S
 import           Data.Vector ((//))
 import qualified Data.Vector as V
-import           Prelude hiding (Word)
+import           Prelude hiding (Word, sum)
 
 type Word = String
 type Sentence = [Word]
@@ -33,6 +33,35 @@ vocabulary = V.fromList . S.toList . S.unions . map S.fromList
 wordVector :: Vocabulary -> Sentence -> V.Vector Int
 wordVector v ws = V.replicate (V.length v) 0 // foldr (\w ps -> let Just i = V.elemIndex w v in (i, 1) : ps) [] ws
 
+addV :: Num a => V.Vector a -> V.Vector a -> V.Vector a
+addV = V.zipWith (+)
+
+addScalar :: Num a => a -> V.Vector a -> V.Vector a
+addScalar x = V.map (+ x)
+
+sumV :: Num a => V.Vector a -> a
+sumV = V.foldr1 (+)
+
+divV :: (Integral a, Fractional b) => V.Vector a -> V.Vector a -> V.Vector b
+divV xs ys = V.zipWith (\x y -> fromIntegral x / fromIntegral y) xs ys
+
+-- cf bayes.trainNB0
+trainNB0 :: [(V.Vector Int, Int)] -> (V.Vector Double, V.Vector Double, Double)
+trainNB0 rows =
+    let columnCount = (V.length . fst . head) rows
+        initial = V.replicate columnCount 0
+        (rowCount, p0Num, p0Denom, p1Num, p1Denom, classTotal) = foldr (\(xs, c) (n, p0Num, p0Denom, p1Num, p1Denom, classTotal) ->
+            if c == 1
+                then (n + 1, p0Num, p0Denom, addV p1Num xs, addScalar (sumV xs) p1Denom, classTotal + c)
+                else (n + 1, addV p0Num xs, addScalar (sumV xs) p0Denom, p1Num, p1Denom, classTotal + c)
+            )
+            (0, initial, initial, initial, initial, 0)
+            rows
+        p0Vector = divV p0Num p0Denom
+        p1Vector = divV p1Num p1Denom
+        pAbusive = fromIntegral classTotal / fromIntegral rowCount
+    in (p0Vector, p1Vector, pAbusive)
+
 main :: IO ()
 main = do
     let v = vocabulary sentences
@@ -41,3 +70,5 @@ main = do
     print $ wordVector v (sentences !! 0)
     print $ sentences !! 3
     print $ wordVector v (sentences !! 3)
+    let r = trainNB0 (zip (map (wordVector v) sentences) classes)
+    print r
