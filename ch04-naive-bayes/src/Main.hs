e305944df104f4774cbd9765cec35ad05055353c
Richard Cook <rcook@rcook.org>
2016-11-26 22:42:49 -0800

    Build word vector from sentence and vocabulary

--- ch04-naive-bayes/src/Main.hs
+++ ch04-naive-bayes/src/Main.hs
@@ -1,4 +1,40 @@
 module Main (main) where
 
+import qualified Data.Set as S
+import           Data.Vector ((//))
+import qualified Data.Vector as V
+import           Prelude hiding (Word)
+
+type Word = String
+type Sentence = [Word]
+type Vocabulary = V.Vector Word
+
+-- cf bayes.loadDataSet
+sentences :: [Sentence]
+sentences =
+    [ ["my", "dog", "has", "flea", "problems", "help", "please"]
+    , ["maybe", "not", "take", "him", "to", "dog", "park", "stupid"]
+    , ["my", "dalmation", "is", "so", "cute", "I", "love", "him"]
+    , ["stop", "posting", "stupid", "worthless", "garbage"]
+    , ["mr", "licks", "ate", "my", "steak", "how", "to", "stop", "him"]
+    , ["quit", "buying", "worthless", "dog", "food", "stupid"]
+    ]
+
+-- cf bayes.loadDataSet
+-- 1 is abusive, 0 not
+classes :: [Int]
+classes = [0, 1, 0, 1, 0, 1]
+
+-- cf bayes.createVocabList
+vocabulary :: [Sentence] -> Vocabulary
+vocabulary = V.fromList . S.toList . S.unions . map S.fromList
+
+-- cf bayes.setOfWords2Vec
+wordVector :: Vocabulary -> Sentence -> V.Vector Int
+wordVector v ws = V.replicate (V.length v) 0 // foldr (\w ps -> let Just i = V.elemIndex w v in (i, 1) : ps) [] ws
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = do
+    let v = vocabulary sentences
+    print $ wordVector v (sentences !! 0)
+    print $ wordVector v (sentences !! 3)
