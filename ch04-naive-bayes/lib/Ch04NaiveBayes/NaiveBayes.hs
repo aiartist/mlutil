2a7c905e75dc273d5f3f0d81549ade31eb5edf65
Richard Cook <rcook@rcook.org>
2016-11-27 00:38:20 -0800

    Tidied up NB classification example

--- ch04-naive-bayes/lib/Ch04NaiveBayes/NaiveBayes.hs
+++ ch04-naive-bayes/lib/Ch04NaiveBayes/NaiveBayes.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Ch04NaiveBayes.NaiveBayes
-    ( NaiveBayesModel (..)
-    , WordVector
+    ( Classification (..)
+    , NaiveBayesModel (..)
     , trainNB0
     , classifyNB
     ) where
@@ -10,7 +10,7 @@ module Ch04NaiveBayes.NaiveBayes
 import qualified Data.Vector as V
 import           MLUtil.Primitives
 
-type WordVector = V.Vector Int
+data Classification = Class0 | Class1 deriving Show
 
 data NaiveBayesModel = NaiveBayesModel
     { nbmP0Vector :: V.Vector Double
@@ -18,16 +18,23 @@ data NaiveBayesModel = NaiveBayesModel
     , nbmPClass1 :: Double
     } deriving Show
 
+-- TODO: Not sure whether the cost of converting from Classification to Int
+-- is worth it just to get type safety...
+classificationToInt :: Classification -> Int
+classificationToInt Class0 = 0
+classificationToInt Class1 = 1
+
 -- cf bayes.trainNB0
-trainNB0 :: [(WordVector, Int)] -> NaiveBayesModel
+trainNB0 :: [(V.Vector Int, Classification)] -> NaiveBayesModel
 trainNB0 rows =
     let columnCount = (V.length . fst . head) rows
         initialNum = V.replicate columnCount 1
         initialDenom = V.replicate columnCount 2
         (rowCount, p0Num, p0Denom, p1Num, p1Denom, classTotal) = foldr (\(xs, c) (n, p0Num, p0Denom, p1Num, p1Denom, classTotal) ->
-            if c == 1
-                then (n + 1, p0Num, p0Denom, addV p1Num xs, addS (sumV xs) p1Denom, classTotal + c)
-                else (n + 1, addV p0Num xs, addS (sumV xs) p0Denom, p1Num, p1Denom, classTotal + c)
+            let c' = classificationToInt c
+            in if c' == 1
+                then (n + 1, p0Num, p0Denom, addV p1Num xs, addS (sumV xs) p1Denom, classTotal + c')
+                else (n + 1, addV p0Num xs, addS (sumV xs) p0Denom, p1Num, p1Denom, classTotal + c')
             )
             (0, initialNum, initialDenom, initialNum, initialDenom, 0)
             rows
@@ -37,9 +44,9 @@ trainNB0 rows =
     in NaiveBayesModel p0Vector p1Vector pAbusive
 
 -- cf bayes.classifyNB
-classifyNB :: NaiveBayesModel -> WordVector -> Int
+classifyNB :: NaiveBayesModel -> V.Vector Int -> Classification
 classifyNB NaiveBayesModel{..} xs =
     let xsF = floatV xs
         p0 = sumV (mulV xsF nbmP0Vector) + log (1.0 - nbmPClass1)
         p1 = sumV (mulV xsF nbmP1Vector) + log nbmPClass1
-    in if p1 > p0 then 1 else 0
+    in if p1 > p0 then Class1 else Class0
--- ch04-naive-bayes/src/Main.hs
+++ ch04-naive-bayes/src/Main.hs
@@ -4,41 +4,31 @@ import           Ch04NaiveBayes.NaiveBayes
 import qualified Data.Set as S
 import           Data.Vector ((//))
 import qualified Data.Vector as V
-import           Prelude hiding (Word, sum)
-
-type Word = String
-type Sentence = [Word]
-type Vocabulary = V.Vector Word
 
 -- cf bayes.loadDataSet
-sentences :: [Sentence]
-sentences =
-    [ ["my", "dog", "has", "flea", "problems", "help", "please"]
-    , ["maybe", "not", "take", "him", "to", "dog", "park", "stupid"]
-    , ["my", "dalmatian", "is", "so", "cute", "I", "love", "him"]
-    , ["stop", "posting", "stupid", "worthless", "garbage"]
-    , ["mr", "licks", "ate", "my", "steak", "how", "to", "stop", "him"]
-    , ["quit", "buying", "worthless", "dog", "food", "stupid"]
+-- Class1 is abusive, Class0 not
+dataSet :: [([String], Classification)]
+dataSet =
+    [ (["my", "dog", "has", "flea", "problems", "help", "please"], Class0)
+    , (["maybe", "not", "take", "him", "to", "dog", "park", "stupid"], Class1)
+    , (["my", "dalmatian", "is", "so", "cute", "I", "love", "him"], Class0)
+    , (["stop", "posting", "stupid", "worthless", "garbage"], Class1)
+    , (["mr", "licks", "ate", "my", "steak", "how", "to", "stop", "him"], Class0)
+    , (["quit", "buying", "worthless", "dog", "food", "stupid"], Class1)
     ]
 
--- cf bayes.loadDataSet
--- 1 is abusive, 0 not
-classes :: [Int]
-classes = [0, 1, 0, 1, 0, 1]
-
 -- cf bayes.createVocabList
-vocabulary :: [Sentence] -> Vocabulary
-vocabulary = V.fromList . S.toList . S.unions . map S.fromList
+vocabulary :: [([String], Classification)] -> V.Vector String
+vocabulary = V.fromList . S.toList . S.unions . map (S.fromList . fst)
 
 -- cf bayes.setOfWords2Vec
-wordVector :: Vocabulary -> Sentence -> WordVector
+wordVector :: V.Vector String -> [String] -> V.Vector Int
 wordVector v ws = V.replicate (V.length v) 0 // foldr (\w ps -> let Just i = V.elemIndex w v in (i, 1) : ps) [] ws
 
 main :: IO ()
 main = do
-    let v = vocabulary sentences
+    let v = vocabulary dataSet
         toVector = wordVector v
-        trainingMatrix = (map toVector sentences)
-        model = trainNB0 (zip trainingMatrix classes)
+        model = trainNB0 (map (\(f, s) -> (toVector f, s)) dataSet)
     print $ classifyNB model (toVector ["love", "my", "dalmatian"])
     print $ classifyNB model (toVector ["stupid", "garbage"])
