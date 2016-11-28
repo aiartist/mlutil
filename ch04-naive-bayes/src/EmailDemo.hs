314a62aee185dbcf698105274d50939da8c586c1
Richard Cook <rcook@rcook.org>
2016-11-28 14:36:32 -0800

    Do not allow invalid byte sequences to fail file reading

--- ch04-naive-bayes/src/EmailDemo.hs
+++ ch04-naive-bayes/src/EmailDemo.hs
@@ -3,11 +3,23 @@ module EmailDemo (runEmailDemos) where
 import           Ch04NaiveBayes.Email
 import           Ch04NaiveBayes.NaiveBayes
 import           Ch04NaiveBayes.Vocabulary
+import           Control.Exception
 import           Data.List
 import           Paths_ch04_naive_bayes
 import           System.Directory
 import           System.FilePath
+import qualified System.IO as IO
 import qualified System.IO.Strict as IOS
+
+readFileWithEncoding :: IO.TextEncoding -> FilePath -> IO String
+readFileWithEncoding encoding path = bracket
+    (IO.openFile path IO.ReadMode >>= \h -> IO.hSetEncoding h encoding >> return h)
+    IO.hClose
+    (IOS.hGetContents)
+
+-- Just encode it: I don't care if we have to discard characters to do it!
+readChar8File :: FilePath -> IO String
+readChar8File = readFileWithEncoding IO.char8
 
 getDataFileNames :: FilePath -> IO [FilePath]
 getDataFileNames dir = do
@@ -18,10 +30,10 @@ getDataFileNames dir = do
 runEmailDemos :: IO ()
 runEmailDemos = do
     spamFileNames <- getDataFileNames "email/spam"
-    spamFileStrs <- mapM IOS.readFile spamFileNames
+    spamFileStrs <- mapM readChar8File spamFileNames
 
     hamFileNames <- getDataFileNames "email/ham"
-    hamFileStrs <- mapM IOS.readFile hamFileNames
+    hamFileStrs <- mapM readChar8File hamFileNames
 
     let spamWordLists = map tokens spamFileStrs
         hamWordLists = map tokens hamFileStrs
