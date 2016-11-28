881b374b6c2589e2d24a0881eb3f4eb5ce1b431a
Richard Cook <rcook@rcook.org>
2016-11-27 16:34:20 -0800

    Build up docList and fullText

--- ch04-naive-bayes/src/EmailDemo.hs
+++ ch04-naive-bayes/src/EmailDemo.hs
@@ -2,6 +2,7 @@ module EmailDemo (runEmailDemos) where
 
 import           Ch04NaiveBayes.Email
 import           Control.Monad
+import           Data.List
 import           Paths_ch04_naive_bayes
 import           System.Directory
 import           System.FilePath
@@ -10,12 +11,32 @@ import qualified System.IO.Strict as IOS
 getDataFileNames :: FilePath -> IO [FilePath]
 getDataFileNames dir = do
     fullDir <- getDataFileName dir
-    fileNames <- listDirectory fullDir
+    fileNames <- sort <$> listDirectory fullDir
     return $ map (fullDir </>) fileNames
+
+type Doc = ([String], Int)
+
+spamClass :: Int
+spamClass = 1
+
+hamClass :: Int
+hamClass = 0
 
 runEmailDemos :: IO ()
 runEmailDemos = do
     spamFileNames <- getDataFileNames "email/spam"
+    spamFileStrs <- mapM IOS.readFile spamFileNames
+
     hamFileNames <- getDataFileNames "email/ham"
-    allWords <- foldM (\ts p -> IOS.readFile p >>= \c -> return $ ts ++ tokens c) [] spamFileNames
-    print allWords
+    hamFileStrs <- mapM IOS.readFile hamFileNames
+
+    let spamWordLists :: [[String]]
+        spamWordLists = map tokens spamFileStrs
+        hamWordLists :: [[String]]
+        hamWordLists = map tokens hamFileStrs
+        docList :: [([String], Int)]
+        docList = map (flip (,) spamClass) spamWordLists ++ map (flip (,) hamClass) hamWordLists
+        fullText :: [String]
+        fullText = concat [concat spamWordLists, concat hamWordLists]
+    print $ length docList
+    print $ length fullText
