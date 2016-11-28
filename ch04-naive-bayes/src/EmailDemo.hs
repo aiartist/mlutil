8ead4167c73598e43925802768dba36122ad4f88
Richard Cook <rcook@rcook.org>
2016-11-27 16:06:47 -0800

    Read words for e-mail data files

--- ch04-naive-bayes/src/EmailDemo.hs
+++ ch04-naive-bayes/src/EmailDemo.hs
@@ -1,12 +1,21 @@
 module EmailDemo (runEmailDemos) where
 
 import           Ch04NaiveBayes.Email
+import           Control.Monad
 import           Paths_ch04_naive_bayes
+import           System.Directory
+import           System.FilePath
 import qualified System.IO.Strict as IOS
+
+getDataFileNames :: FilePath -> IO [FilePath]
+getDataFileNames dir = do
+    fullDir <- getDataFileName dir
+    fileNames <- listDirectory fullDir
+    return $ map (fullDir </>) fileNames
 
 runEmailDemos :: IO ()
 runEmailDemos = do
-    path <- getDataFileName "email/ham/6.txt"
-    s <- IOS.readFile path
-    --let s = "This book is the best book on Python or M.L. I have ever laid eyes upon."
-    print $ tokens s
+    spamFileNames <- getDataFileNames "email/spam"
+    hamFileNames <- getDataFileNames "email/ham"
+    allWords <- foldM (\ts p -> IOS.readFile p >>= \c -> return $ ts ++ tokens c) [] spamFileNames
+    print allWords
