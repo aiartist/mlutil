d494c456652339151c0a08f4fad679abcea66d5e
Richard Cook <rcook@rcook.org>
2016-11-28 17:04:53 -0800

    Implement choiceExtract and choiceExtractN

--- mlutil/spec/RandomSpec.hs
+++ mlutil/spec/RandomSpec.hs
@@ -8,9 +8,57 @@ import           Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "dummy" $ do
-        it "passes" $
-            1 `shouldBe` 1
+    describe "choiceExtract" $ do
+        it "extracts random elements" $ do
+            let xs = [10, 20, 30]
+
+            Just (xs', x') <- choiceExtract xs
+            length xs' `shouldBe` 2
+            x' `shouldSatisfy` (`elem` xs)
+
+            Just (xs'', x'') <- choiceExtract xs'
+            length xs'' `shouldBe` 1
+            x'' `shouldSatisfy` (\x -> x `elem` xs && x /= x')
+
+            Just (xs''', x''') <- choiceExtract xs''
+            length xs''' `shouldBe` 0
+            x''' `shouldSatisfy` (\x -> x `elem` xs && x /= x' && x /= x'')
+
+        it "fails on empty list" $ do
+            r <- choiceExtract [] :: IO (Maybe ([Int], Int))
+            r `shouldBe` Nothing
+
+    describe "choiceExtractN" $ do
+        it "returns original list and empty if n is 0 and list is empty" $ do
+            Just (xs, ys) <- choiceExtractN 0 [] :: IO (Maybe ([Int], [Int]))
+            xs `shouldBe` []
+            ys `shouldBe` []
+
+        it "returns original list and empty if n is 0 and list is non-empty" $ do
+            Just (xs, ys) <- choiceExtractN 0 [10, 20, 30]
+            xs `shouldBe` [10, 20, 30]
+            ys `shouldBe` []
+
+        it "fails for n > 0 if list is empty" $ do
+            r <- choiceExtractN 1 [] :: IO (Maybe ([Int], [Int]))
+            r `shouldBe` Nothing
+
+        it "fails if n is greater than length of list" $ do
+            r <- choiceExtractN 3 [10, 20]
+            r `shouldBe` Nothing
+
+        it "fails if n is negative" $ do
+            r <- choiceExtractN (-1) [10, 20]
+            r `shouldBe` Nothing
+
+        it "extracts n random elements" $ do
+            let xs = [10, 20, 30, 40]
+
+            Just (xs', x') <- choiceExtractN 2 xs
+            length xs' `shouldBe` 2
+            x' !! 0 `shouldSatisfy` (`elem` xs)
+            x' !! 1 `shouldSatisfy` (`elem` xs)
+            x' !! 0 `shouldNotBe` x' !! 1
 
 main :: IO ()
 main = hspec spec
--- mlutil/src/MLUtil/Random.hs
+++ mlutil/src/MLUtil/Random.hs
@@ -1,1 +1,27 @@
-module MLUtil.Random () where
+module MLUtil.Random
+    ( choiceExtract
+    , choiceExtractN
+    ) where
+
+import           Control.Monad
+import           MLUtil.Util
+import           System.Random
+
+-- |Removes and returns random element from list
+choiceExtract :: [a] -> IO (Maybe ([a], a))
+choiceExtract [] = return Nothing
+choiceExtract xs = randomRIO (0, length xs - 1) >>= return . (flip removeAt) xs
+
+-- |Removes and returns n random elements from list
+choiceExtractN :: Int -> [a] -> IO (Maybe ([a], [a]))
+choiceExtractN 0 xs = return $ Just (xs, [])
+choiceExtractN _ [] = return Nothing
+choiceExtractN n xs
+    | n < 0 || n >= length xs = return Nothing
+    | otherwise = do
+        x <- foldM (\(xs', ys) _ -> do
+                Just (xs'', y) <- choiceExtract xs'
+                return (xs'', y : ys))
+                (xs, [])
+                [0 .. n - 1]
+        return $ Just x
