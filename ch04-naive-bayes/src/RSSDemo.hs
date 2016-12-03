b12f601f4c3e28f813a989986d6fdb2e8e1b4b3d
Richard Cook <rcook@rcook.org>
2016-12-03 11:09:08 -0800

    Move into rssEntries function

--- ch04-naive-bayes/src/RSSDemo.hs
+++ ch04-naive-bayes/src/RSSDemo.hs
@@ -7,12 +7,22 @@ import           Network.Wreq
 import           Text.Feed.Import
 import           Text.Feed.Query
 
-runRSSDemos :: IO ()
-runRSSDemos = do
-    r <- get "http://newyork.craigslist.org/stp/index.rss"
+nyUrl :: String
+nyUrl = "http://newyork.craigslist.org/stp/index.rss"
+
+sfUrl :: String
+sfUrl = "http://sfbay.craigslist.org/stp/index.rss"
+
+rssEntries :: String -> IO [String]
+rssEntries url = do
+    r <- get url
     let c = C8.unpack $ r ^. responseBody
         Just feed = parseFeedString c
+    return $ map (\item -> let Just summary = getItemSummary item in summary) (feedItems feed)
 
-    forM_ (take 1 $ feedItems feed) $ \item -> do
-        let summary = getItemSummary item
-        print summary
+runRSSDemos :: IO ()
+runRSSDemos = do
+    nyEntries <- rssEntries nyUrl
+    sfEntries <- rssEntries sfUrl
+    print $ head nyEntries
+    print $ head sfEntries
