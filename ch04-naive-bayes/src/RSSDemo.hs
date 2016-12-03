6ca55b9322819178b866a748c8a2b41a9f468579
Richard Cook <rcook@rcook.org>
2016-12-03 11:04:05 -0800

    Read RSS feed

--- ch04-naive-bayes/src/RSSDemo.hs
+++ ch04-naive-bayes/src/RSSDemo.hs
@@ -1,4 +1,18 @@
 module RSSDemo (runRSSDemos) where
 
+import           Control.Lens
+import           Control.Monad
+import qualified Data.ByteString.Lazy.Char8 as C8
+import           Network.Wreq
+import           Text.Feed.Import
+import           Text.Feed.Query
+
 runRSSDemos :: IO ()
-runRSSDemos = return ()
+runRSSDemos = do
+    r <- get "http://newyork.craigslist.org/stp/index.rss"
+    let c = C8.unpack $ r ^. responseBody
+        Just feed = parseFeedString c
+
+    forM_ (take 1 $ feedItems feed) $ \item -> do
+        let summary = getItemSummary item
+        print summary
