6b14557ebb996a4d1606b436f16334bad0238b70
Richard Cook <rcook@rcook.org>
2016-12-07 01:09:53 -0800

    Plot best-fit line

--- ch05-logistic-regression/src/GradientAscentDemo.hs
+++ ch05-logistic-regression/src/GradientAscentDemo.hs
@@ -8,7 +8,7 @@ import qualified Data.Vector.Unboxed as VU
 import           MLUtil
 import           MLUtil ((|||))
 import           MLUtil.Graphics hiding ((|||))
-import           Graphics.Rendering.Chart.Easy
+import           Graphics.Rendering.Chart.Easy hiding (Matrix)
 
 waveform :: (Double -> Double) -> [Double] -> [(Double, Double)]
 waveform f xs = [ (x, f x) | x <- xs ]
@@ -31,6 +31,29 @@ createSigmoidFigures = do
         , clYAxisLabel = Just "sigmoid(x)"
         }
         [ mkRPlot (line "sigmoid(x)" [waveform sigmoid [-60.0, -59.0 .. 60.0]]) ]
+
+    Just m@LabelledMatrix{..} <- getDataFileName "testSet.txt" >>= readLabelledMatrix
+    let values = ones (rows lmValues) 1 ||| lmValues
+        labels = col (map fromIntegral (VU.toList lmLabelIds))
+        r = gradAscent 0.001 500 values labels
+        plots = (mkRPlot $ line "best-fit line" [waveform (bestFitLine r) [-3.0, -2.9 .. 3.0]])
+                    : (colouredSeriesPlots m 0 1)
+    renderChartSVG
+        "sigmoid-fig5-4.svg"
+        defaultChartLabels
+        { clTitle = Just "Figure 5.4: Logistic regression best-fit line after gradient ascent"
+        , clXAxisLabel = Just "x"
+        , clYAxisLabel = Just "sigmoid(x)"
+        }
+        plots
+
+-- cf logRegres.plotBestFit
+bestFitLine :: Matrix R -> Double -> Double
+bestFitLine weights x =
+    let w0 = weights `atIndex` (0, 0)
+        w1 = weights `atIndex` (1, 0)
+        w2 = weights `atIndex` (2, 0)
+    in ((-w0) - w1 * x) / w2
 
 testGradAscent :: IO ()
 testGradAscent = do
