9b040bfeafbb95021d10e2b54f9a7f6e219f1429
Richard Cook <rcook@rcook.org>
2016-12-11 11:27:36 -0800

    Add tests for elementwise sum

--- easyla/spec/EasySpec.hs
+++ easyla/spec/EasySpec.hs
@@ -8,9 +8,21 @@ import           Test.Hspec
 
 spec :: Spec
 spec = do
+    describe "vector +" $
+        it "performs elementwise add" $
+            vector [1, 2, 3] + vector [3, 0, -2] `shouldBe` vector [4.0, 2.0, 1.0]
+
     describe "vector *" $
         it "performs elementwise product" $
             vector [1, 2, 3] * vector [3, 0, -2] `shouldBe` vector [3.0, 0.0, -6.0]
+
+    describe "matrix +" $
+        it "performs elementwise add" $
+            matrix 3 [1..9] + ident 3 `shouldBe` (3 >< 3)
+                                                    [ 2.0, 2.0, 3.0
+                                                    , 4.0, 6.0, 6.0
+                                                    , 7.0, 8.0, 10.0
+                                                    ]
 
     describe "matrix *" $
         it "performs elementwise product" $
